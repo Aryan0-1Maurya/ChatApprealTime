ef52bdbbcf8d51158cb5659190ce5213fdea86c8
Aryan Maurya <136960772+Aryan0-1Maurya@users.noreply.github.com>
2023-10-24 13:58:57 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,6 @@
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD_6PHNu3iyqE_zU1n9x_SL12M4Rm_Vg9o",
-    authDomain: "article-search-e-1695555651666.firebaseapp.com",
-    databaseURL: "https://worldchatapp-devxprite-default-rtdb.firebaseio.com",
-    projectId: "article-search-e-1695555651666",
-    storageBucket: "article-search-e-1695555651666.appspot.com",
-    messagingSenderId: "824371932353",
-    appId: "1:824371932353:web:235f0f29e88fb144a3b840",
-    measurementId: "G-F8RPDNK93G",
+   
 };
 
 // Initialize Firebase
